7bcd9394f05d6a6ca745cf3b1f9d2606e38db27d
Sabhya Gupta <sabhyagupta4xr@gmail.com>
2025-10-30 18:57:28 +0530

    modified the popup color to darker version

--- popup.js
+++ popup.js
@@ -26,10 +26,20 @@ themeSelect.addEventListener('change', () => {
 // Helper function to send the command to the content script
 function sendMessage(enabled, theme) {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      action: "applyTheme",
-      enabled: enabled,
-      theme: theme
-    });
+    
+    // Make sure we actually found a tab
+    if (tabs[0] && tabs[0].id) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        action: "applyTheme",
+        enabled: enabled,
+        theme: theme
+      })
+      .catch(error => {
+        // This new .catch() block fixes the error.
+        // It silences the "Receiving end does not exist" message.
+        console.log("Target page is not a PDF, ignoring.", error.message);
+      });
+    }
+
   });
 }
